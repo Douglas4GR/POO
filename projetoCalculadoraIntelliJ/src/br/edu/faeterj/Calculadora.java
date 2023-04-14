f6e64153ef725510cad0e76e856f1463718ac208
Douglas Garcia <douglasgovmad@gmail.com>
2023-04-14 02:36:31 -0300

    add opc n abrir, opc reutilizar, modf para double

--- projetoCalculadoraIntelliJ/src/br/edu/faeterj/Calculadora.java
+++ projetoCalculadoraIntelliJ/src/br/edu/faeterj/Calculadora.java
@@ -5,54 +5,133 @@ public class Calculadora {
 
     public void mostrarOpc()
     {
-        System.out.println("Selecione uma opcao: ");
-        System.out.println("0 - Soma");
-        System.out.println("1 - Subtracao");
-        System.out.println("2 - Multiplicacao");
-        System.out.println("3 - Divisao");
-        System.out.println("4 - Sair do programa");
+        System.out.println("Selecione uma opcao: "); //menu
+        System.out.println("0 - Sair do programa"); //sair
+        System.out.println("1 - Soma"); //a+b
+        System.out.println("2 - Subtracao"); //a-b
+        System.out.println("3 - Multiplicacao"); //a*b
+        System.out.println("4 - Divisao"); //a/b
+        System.out.println("5 - Potencia"); //a^b
+        System.out.println("6 - Raiz"); //sqrt(a)
+        System.out.println("7 - Fatorial"); //a!
+        System.out.println("8 - Logaritmo"); //log(a)
+        System.out.println("9 - Seno"); //sen(a)
+        System.out.println("10 - Cosseno"); //cos(a)
+        System.out.println("11 - Tangente"); //tan(a)
     }
 
-    public int Selec(int opcao, int valor1, int valor2) {
+    public double Selec(int opcao, double valor1, double valor2) {
 
-        int resultadoInt = 0;
+        double resultadoInt = 0;
 
         switch(opcao) {
-            case 0:
+            case 1:
                 resultadoInt = Soma(valor1, valor2);
                 break;
-            case 1:
+            case 2:
                 resultadoInt = Subt(valor1, valor2);
                 break;
-            case 2:
+            case 3:
                 resultadoInt = Mult(valor1, valor2);
                 break;
-            case 3:
+            case 4:
                 resultadoInt = Divi(valor1, valor2);
+                break;
+            case 5:
+                resultadoInt = Pot(valor1, valor2);
+                break;
+            case 6:
+                resultadoInt = Raiz(valor1);
+                break;
+            case 7:
+                resultadoInt = Fatorial(valor1);
+                break;
+            case 8:
+                resultadoInt = Log(valor1);
+                break;
+            case 9:
+                resultadoInt = Seno(valor1);
+                break;
+            case 10:
+                resultadoInt = Cosseno(valor1);
+                break;
+            case 11:
+                resultadoInt = Tangente(valor1);
+                break;
+            default:
+                System.out.println("Opcao invalida");
                 break;
         }
         return resultadoInt;
     }
 
     //operacoes
-    public int Soma(int a,int b)
+    public double Soma(double a,double b)
     {
         return a+b;
     }
 
-    public int Subt(int a,int b)
+    public double Subt(double a,double b)
     {
         return a-b;
     }
 
-    public int Mult(int a,int b)
+    public double Mult(double a,double b)
     {
         return a*b;
     }
 
-    public int Divi(int a,int b)
+    public double Divi(double a,double b)
     {
         return a/b;
     }
+
+    public double Pot(double a,double b)
+    {
+        double pot = Math.pow(a, b);
+        return pot;
+    }
+
+    public double Raiz(double a)
+    {
+        double sqrt = Math.sqrt(a);
+        return sqrt;
+    }
+
+    public double Fatorial(double a)
+    {
+        double fat = 1;
+        for(int i = 1; i <= a; i++)
+        {
+            fat = fat * i;
+        }
+        return fat;
+    }
+
+    public double Log(double a)
+    {
+        double log = Math.log(a);
+        return log;
+    }
+
+    public double Seno(double a)
+    {
+        double seno = Math.sin(a);
+        return seno;
+    }
+
+    public double Cosseno(double a)
+    {
+        double cosseno = Math.cos(a);
+        return cosseno;
+    }
+
+    public double Tangente(double a)
+    {
+        double tangente = Math.tan(a);
+        return tangente;
+    }
+
+    //fim operacoes
 
 }
--- projetoCalculadoraIntelliJ/src/br/edu/faeterj/Main.java
+++ projetoCalculadoraIntelliJ/src/br/edu/faeterj/Main.java
@@ -8,30 +8,40 @@ public class Main {
 
         //agora os comentarios serao feitos em portugues, comecei a utilizar o intellij
         //declaração de variaveis
-        int valor1 = 0;
-        int valor2 = 0;
-        int opcao = 0;
-
-        //coletando primeiro numero (necessario)
-        System.out.println("Valor 1: ");
-        valor1 = sc.nextInt();
+        double valor1;
+        double valor2;
+        double resultado = 0;
+        int opcao = 1;
 
         //começando operações
-        while(opcao < 4) {
+        while(opcao > 0) {
 
             //escolher operação
             calc.mostrarOpc();
             opcao = sc.nextInt();
 
-            if(opcao > 3) {
+            if(opcao < 1) {
                 System.out.println("Calculadora finalizada");
-            }else {
-                //coletando valores
-                System.out.println("Valor 2: ");
-                valor2 = sc.nextInt();
-                //fazendo operação (valor1, agora o resultado sera usado enquanto o usuario quiser)
-                valor1 = calc.Selec(opcao, valor1, valor2);
-                System.out.println("Resultado: "+ valor1);
+            } else {
+                //coletando primeiro numero (necessario)
+                System.out.println("Valor 1(ou digite 0 para usar o resultado da operação anterior): ");
+                valor1 = sc.nextDouble();
+
+                if(valor1 == 0) {
+                    //usando resultado anterior
+                    valor1 = resultado;
+                }
+                if (opcao >= 6) {
+                    valor2 = 0;
+                } else {
+                    //coletando segundo numero
+                    System.out.println("Valor 2: ");
+                    valor2 = sc.nextDouble();
+                }
+
+                //fazendo operação
+                resultado = calc.Selec(opcao, valor1, valor2);
+                System.out.println("Resultado: "+ resultado);
             }
         }
         sc.close();

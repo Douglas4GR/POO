ae97af4b0dd2724d58a0a98b1062ad5726913836
Douglas Garcia Rangel <109640187+DouglasGR99@users.noreply.github.com>
2023-04-22 23:51:08 -0300

    craindo metodos criarProduto e mostrarLProduto

--- projetoLojaProduto/src/br/edu/faeterj/Main.java
+++ projetoLojaProduto/src/br/edu/faeterj/Main.java
@@ -7,30 +7,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ArrayList<Produto> lproduto = new ArrayList();
 		Scanner sc = new Scanner(System.in);
-		
-		// criar mais produtos
-		// passar as funções para classe produto (metodos)
-		
-		Produto prod = new Produto();
-		System.out.println("Digite as info do produto:");
-		
-		System.out.println("nome:");
-		prod.nome = sc.next();
-		
-		System.out.println("valor:");
-		prod.valor = sc.nextDouble();
-		
-		System.out.println("quantidade:");
-		prod.quantidade = sc.nextInt();
-		
-		System.out.println("descricao:");
-		prod.descricao = sc.next();
-		
-		System.out.println("cod barra:");
-		prod.codBarra = sc.nextInt();
-		
-		System.out.println("nome: " + prod.nome +" valor:" + prod.valor + " quantidade:" + prod.quantidade + " descricao:" + prod.descricao + " cod barra:" + prod.codBarra);
+		ArrayList<Produto> lproduto = new ArrayList<>();
+		int tamanhoLista = 0;
+
+		System.out.println("Digite o tamanho da lista:");
+		tamanhoLista = sc.nextInt();
+		for (int i = 0; i < tamanhoLista; i++) {
+			Produto prod = new Produto();
+			prod.criarProduto();
+			lproduto.add(prod);
+		}
+		Produto exibir = new Produto();
+		exibir.mostrarLProduto(lproduto);
 	}
 }

0fdd0a668b1daa15f6ffb33de738d88268553e60
Douglas Garcia Rangel <109640187+DouglasGR99@users.noreply.github.com>
2023-07-11 15:45:01 -0300

    Adição da pasta do curso de java da oracle

---
+++ Curso Oracle/session 2/ProblemSet2_1/ProblemSet2_1.java
@@ -0,0 +1,16 @@
+public class ProblemSet2_1 {
+    public static void main(String[] args) {
+        //Use 8 print statements to print a smiley face.
+        //Your art will rely on only a single character, besides space, such as X or #.
+        
+        System.out.println("- - # # # # - -");
+        System.out.println("- # - - - - # -");
+        System.out.println("# - # - - # - #");
+        System.out.println("# - - - - - - #");
+        System.out.println("# - # - - # - #");
+        System.out.println("# - - # # - - #");
+        System.out.println("- # - - - - # -");
+        System.out.println("- - # # # # - -");
+        
+    }   
+}
---
+++ Curso Oracle/session 2/ProblemSet2_2/ProblemSet2_2.java
@@ -0,0 +1,14 @@
+public class ProblemSet2_2 {
+    public static void main(String[] args) {
+        //Use print statements to create your own beautiful original ASCII art.
+        //Use comments to describe what your image is depicting.
+        
+        // ainda nao sei o que é isso
+
+        System.out.println("- - - - - - - - -");
+        System.out.println("- - -_______- - -");
+        System.out.println("- - ( o   o ) - -");
+        System.out.println("- - |   v   | - -");
+        System.out.println("- - /       / - -");       
+    } 
+}

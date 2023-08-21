5f9943709cffff8f69de9597bdcf21680ee360de
Douglas Garcia Rangel <109640187+DouglasGR99@users.noreply.github.com>
2023-08-21 00:55:29 -0300

    inclusão do projeto do cachorro serializado

---
+++ Serialização cachorro/src/CachorroPKG/Cachorro.java
@@ -0,0 +1,5 @@
+package CachorroPKG;
+
+public class Cachorro {
+
+}
---
+++ Serialização cachorro/src/CachorroPKG/SerializaCachorro.java
@@ -0,0 +1,10 @@
+package CachorroPKG;
+
+public class SerializaCachorro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

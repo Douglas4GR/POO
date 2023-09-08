efdde46b8e54e57941f2604daed246c00a8782ce
Douglas Garcia Rangel <109640187+DouglasGR99@users.noreply.github.com>
2023-09-07 23:18:11 -0300

    modificação de arquivos pequenos

--- Serialização cachorro/src/CachorroPKG/SerializaCachorro.java
+++ Serialização cachorro/src/CachorroPKG/SerializaCachorro.java
@@ -1,10 +1,41 @@
 package CachorroPKG;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class SerializaCachorro {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Cachorro dog = new Cachorro("Bob", 2021, "(21)98765-4321");
+		
+		
+		try (FileOutputStream fileOut = new FileOutputStream("dog.ser");
+	             ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
+			
+	            out.writeObject(dog);
+	            System.out.println("Cachorro serializado com sucesso!");
+	            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+		
+	        try (FileInputStream fileIn = new FileInputStream("dog.ser");
+	             ObjectInput in = new ObjectInputStream(fileIn)) {
+	        	
+	            Cachorro cachorroDesserializado = (Cachorro) in.readObject();
+	            System.out.println("Cachorro desserializado:");
+	            System.out.println("Nome: " + cachorroDesserializado.getNome());
+	            System.out.println("Ano de nascimento: " + cachorroDesserializado.getDataNasc());
+	            System.out.println("Telefone do dono: " + cachorroDesserializado.getTelefoneDono());
+	            
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 	}
 
 }

cffe6c4d1eba13641ba01d8c5781166f36bfa4d8
Douglas Garcia Rangel <109640187+DouglasGR99@users.noreply.github.com>
2024-04-30 01:13:58 -0300

    Criando tabelas carga temporaria e clientes

--- 5SBD/SQLQueryBazarHaveAll.sql
+++ 5SBD/SQLQueryBazarHaveAll.sql
@@ -1,1 +1,44 @@
 CREATE DATABASE Bazar_Have_All;
+
+USE Bazar_Have_All;
+
+CREATE TABLE Carga_Temporaria (
+    OrderId INT NOT NULL,
+    OrderItemId INT NOT NULL,
+    PurchaseDate DATETIME NOT NULL,
+    PaymentsDate DATETIME,
+    BuyerEmail VARCHAR(200),
+    BuyerName VARCHAR(140),
+    Cpf VARCHAR(11),
+    BuyerPhoneNumber VARCHAR(20),
+    Sku VARCHAR(20) NOT NULL,
+    ProductName VARCHAR(140),
+    QuantityPurchased INT,
+    Currency VARCHAR(140),
+    ItemPrice DECIMAL(10, 2),
+    ShipServiceLevel VARCHAR(140),
+    RecipientName VARCHAR(140),
+    ShipAddress1 VARCHAR(140),
+    ShipAddress2 VARCHAR(140),
+    ShipAddress3 VARCHAR(140),
+    ShipCity VARCHAR(140),
+    ShipState VARCHAR(140),
+    ShipPostalCode VARCHAR(20),
+    ShipCountry VARCHAR(40),
+    IossNumber INT,
+);
+
+CREATE TABLE Clientes (
+    ClientId INT PRIMARY KEY,
+    ClientEmail VARCHAR(200) NOT NULL,
+    ClientName VARCHAR(140) NOT NULL,
+    Cpf VARCHAR(11) NOT NULL,
+    ClientPhoneNumber VARCHAR(20),
+    ClientAddress1 VARCHAR(140),
+    ClientAddress2 VARCHAR(140),
+    ClientAddress3 VARCHAR(140),
+    ClientCity VARCHAR(140),
+    ClientState VARCHAR(140),
+    ClientPostalCode VARCHAR(20),
+    ClientCountry VARCHAR(40)
+);
